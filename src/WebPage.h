7443927bf66b61f31fcdc4cc7493b82a2c55d40f
sivar2311 <sivar2311@gmail.com>
2022-09-23 17:04:32 +0200

    improved webpage (thanks to kakopappa)

--- src/WebPage.h
+++ src/WebPage.h
@@ -2,60 +2,110 @@
 
 const char* webPage = R"***(
 <html>
-
 <head>
-  <meta charset='utf-8'>
-  <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-  <title>WebSerial</title>
-  <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>WebSerial</title>
 
-  <style>
-    textarea{
-      width:100%;
-      background-color: lightgrey;
-      font-family: 'Courier New', monospace;
-    }
-  </style>
+    <style>
+        body {
+            font-family: 'verdana';
+        }
+
+        .wrapper {
+            width: 100%;
+            max-width: 960px;
+            margin: 0 auto;
+        }
+
+        .header {
+            height: 40px;
+        }
+
+        .main {
+            height: calc(100% - 88px);
+        }
+
+        .footer {
+            height: 48px;
+            width: 100%;
+            max-width: 960px;
+            position: fixed;
+            bottom:0;
+        }
+
+        textarea {
+            background-color: lightgray;
+            font-family: 'Courier New', monospace;
+            color: #222222;
+            width: 100%;
+            max-width: 960px;
+            height: 100%;
+
+        }
+    </style>
 </head>
 
 <body>
-  <h1>WebSerial</h1>
-  <form>
-    <div>
-      <textarea readonly id="messages" rows="20" cols="80"></textarea>
-    </div>
-    <div>
-      <input type="checkbox" id="scroll" value="scroll" checked="true"><label for="checkbox">auto scroll</label>
-      <input type="checkbox" id="timestamp" value="timestamp"><label for="timestamp">timestamp</label>
-      <button type="button" onclick="clear_lines()">clear</button>
-    </div>
-  </form>
+    <form onsubmit="return false">
+        <div class="wrapper">
+            <div class="header">
+                <h1>WebSerial</h1>
+            </div>
+            <div class="main">
+                <textarea readonly id="messages"></textarea>
+            </div>
+            <div class="footer">
+                <div>
+                    <input type="checkbox" id="scroll" value="scroll" checked="true"><label for="checkbox">auto
+                        scroll</label>
+                    <input type="checkbox" id="timestamp" value="timestamp"><label for="timestamp">timestamp</label>
+                    <button type="button" onclick="clear_lines()">clear</button>
+                </div>
+                <div>
+                    <input type="text" id="text"><button type="button" onclick="send_line()" autofocus>send</button>
+                </div>
+            </div>
+        </div>
+    </form>
 </body>
 <script>
-  let scroll = document.getElementById('scroll');
-  let timestamp = document.getElementById('timestamp');
-  let messages = document.getElementById('messages');
+    let scroll = document.getElementById('scroll');
+    let timestamp = document.getElementById('timestamp');
+    let messages = document.getElementById('messages');
+    let send_text = document.getElementById('text');
 
-  const ws_url = "ws://" + window.location.hostname + "/webserialws";
-  socket = new WebSocket(ws_url);
+    text.addEventListener("keypress", event => {
+        if (event.key === "Enter") send_line();
+    });
 
-  socket.onmessage = (event) => {
-    var next_line = event.data;
-    const timeElapsed = Date.now();
-    const today = new Date(Date.now());
-    if (timestamp.checked) {
-      const current_time = new Date(Date.now());
-      next_line = "[" + current_time.toISOString() + "]: " + next_line;
+    const port = 80;
+    const url = "/webserialws";
+
+    const ws_url = `ws://${window.location.hostname}:${window.location.port}/webserialws`;
+
+    socket = new WebSocket(ws_url);
+
+    socket.onmessage = (event) => {
+        var next_line = event.data;
+
+        if (timestamp.checked) {
+            const current_time = new Date(Date.now()).toLocaleTimeString();
+            next_line = `[${current_time}]: ` + next_line;
+        }
+        messages.value = messages.value + next_line;
+
+        if (scroll.checked) messages.scrollTop = messages.scrollHeight;
     }
-    messages.value = messages.value + next_line;
-    if (scroll.checked) messages.scrollTop = messages.scrollHeight;
-  }
 
-  var clear_lines = function () {
-    messages.value = "";
-  }
+    var clear_lines = function () {
+        messages.value = "";
+    }
 
+    var send_line = function () {
+        socket.send(text.value);
+        text.value = "";
+    }
 </script>
-
-</html>
-)***";
+</html>)***";
