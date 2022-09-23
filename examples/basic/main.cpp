3759e465af0bc62c5471f884024f388295545fae
sivar2311 <sivar2311@gmail.com>
2022-09-23 17:03:45 +0200

    Improved basic example

--- examples/basic/main.cpp
+++ examples/basic/main.cpp
@@ -1,12 +1,13 @@
 #include <Arduino.h>
-
 #include <WebSerial.h>
 #include <WiFi.h>
 
 const char* wifi_ssid = "your_wifi_ssid";
 const char* wifi_pass = "your_wifi_pass";
+const int   port      = 80;
+const char* url       = "/webserial";
 
-AsyncWebServer server(80);
+AsyncWebServer server(port);
 
 void setup() {
     Serial.begin(115200);
@@ -18,13 +19,17 @@ void setup() {
     }
     Serial.println("connected.");
 
-    WebSerial.begin(&server);
+    WebSerial.begin(&server, url);
     server.begin();
 
-    Serial.printf("Open your browser and navigate to http://%s/webserial\r\n", WiFi.localIP().toString().c_str());
+    if (port == 80)
+        Serial.printf("Open your webbrowser and navigate to http://%s%s\r\n", WiFi.localIP().toString().c_str(), url);
+    else
+        Serial.printf("Open your webbrowser and navigate to http://%s:%d%s\r\n", WiFi.localIP().toString().c_str(), port, url);
 }
 
 void loop() {
     WebSerial.printf("uptime: %d\r\n", millis() / 1000);
+    Serial.printf("uptime: %d\r\n", millis() / 1000);
     delay(1000);
 }
